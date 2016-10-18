446ef07c2f2136ebffbd06d7ee9c53b71706fa72
dilee5 <dillon.leec17@gmail.com>
2016-10-18 14:16:15 -0700

    Dillon's Farm

--- Farm.java
+++ Farm.java
@@ -3,15 +3,17 @@ class Farm
    private Animal[] gangOfAnimals = new Animal[3];
    public Farm()
    {
-   	gangOfAnimals[0] = new Cow("cow","moo");
+   	gangOfAnimals[0] = new NamedCow("cow", "Elsie", "moo");
    	gangOfAnimals[1] = new Chick("chick", "cheep","cluck");
    	gangOfAnimals[2] = new Pig("pig","oink");
    }
-   public void animalSound()
+   public void getSound()
    {
    		for (int v = 0; v < gangOfAnimals.length; v++)
    		{
-   			System.out.println(gangOfAnimals[v].getType() + "goes " + gangOfAnimals[v].getSound());
+   			System.out.println(gangOfAnimals[v].getType() + " goes " + gangOfAnimals[v].getSound());
    		}
+   		System.out.println("The cow is known as " + ((NamedCow)gangOfAnimals[0]).getName());
+
    }
 }
--- NamedCow.java
+++ NamedCow.java
@@ -1,4 +1,16 @@
-class NamedCow
+class NamedCow extends Cow
 {
-
+	private String myType;
+	private String mySound;
+	private String myCowName;
+	public NamedCow(String type, String name, String sound)
+	{
+		myType = type;
+		mySound = sound;
+		myCowName = name;
+	}
+	public String getName()
+	{
+		return myCowName;
+	}
 }
